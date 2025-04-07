ce81a709e25b0a911dd210e0369c72263334a4b7
Nyasha Shepard Ushewokunze <mrshepard18@gmail.com>
2025-04-07 19:14:17 +0200

    fix: Correct vite base path and commit dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/-Final-Project-Work/', // Set the base path for GitHub Pages
+  base: '/Final-Project-Work/', // Set the base path for GitHub Pages
   build: {
     rollupOptions: {
       input: {
